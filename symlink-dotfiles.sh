b91d48af27109f8a85d8c274faedb2473722ec56
Manvil George <manvil.george@greensync.com.au>
2019-12-03 11:08:37 +1100

    Adding tig

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -6,4 +6,5 @@ if [ -f ~/.vimrc ]; then mv ~/.vimrc ~/.vimrc_backup; fi; ln -s $DIR/vim/.vimrc 
 if [ -f ~/.zshrc ]; then mv ~/.zshrc ~/.zshrc_backup; fi; ln -s $DIR/zsh/.zshrc ~/.zshrc
 if [ -f ~/.oh-my-zsh/custom ] ; then rm -rf ~/.oh-my-zsh/custom; fi; ln -s $DIR/zsh/custom/ ~/.oh-my-zsh/custom
 if [ -f ~/.irbrc ]; then mv ~/.irbrc ~/.irbrc_backup; fi; ln -s $DIR/irb/.irbrc ~/.irbrc
+if [ -f ~/.tigrc ]; then mv ~/.trigrc ~/.tigrc_backup; fi; ln -s $DIR/tig/.tigrc/ ~/.tigrc
 echo 1
